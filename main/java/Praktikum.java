716e5339e2f8f1158380cbc5d3f8f08dbfa715a4
MartynovaAnna <93049645+MartynovaAnna@users.noreply.github.com>
2022-01-13 19:47:45 +0300

    Delete main/java directory

--- main/java/Praktikum.java
+++
@@ -1,9 +0,0 @@
-public class Praktikum {
-
-    public static void main(String[] args) {
-
-        String someName = "Тимоти Шаламе";
-        Account account = new Account(someName);
-        account.checkNameToEmboss();
-    }
-}
