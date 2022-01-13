e282210c72a627640b91447e3a9d30bbea190f6d
MartynovaAnna <93049645+MartynovaAnna@users.noreply.github.com>
2022-01-13 19:49:39 +0300

    Add files via upload

---
+++ src/main/java/Praktikum.java
@@ -0,0 +1,9 @@
+public class Praktikum {
+
+    public static void main(String[] args) {
+
+        String someName = "Тимоти Шаламе";
+        Account account = new Account(someName);
+        account.checkNameToEmboss();
+    }
+}
