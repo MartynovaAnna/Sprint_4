49c83606cc09c667e6551203beae8d0048b57477
MartynovaAnna <93049645+MartynovaAnna@users.noreply.github.com>
2022-01-14 19:05:41 +0300

    Update Account.java

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,18 +1,13 @@
 public class Account {
 
-    private String name;
+    private final String name;
 
     public Account(String name) {this.name = name;}
 
     public boolean checkNameToEmboss() {
+        if (name == null) {return false;}
+
         String newName = name.replace(" ", "");
-        boolean result;
-        if (name.length() >= 3 && name.length() <= 19 && name.equals(name.trim()) && name.length() - newName.length() == 1) {
-            result = true;
-        }
-        else {
-            result = false;
-        }
-        return result;
-        }
+        return (name.length() >= 3 && name.length() <= 19 && name.equals(name.trim()) && name.length() - newName.length() == 1);
     }
+}
